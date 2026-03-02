26f550c00c377929e957e46924782c78cc9375dd
MadsPalu <73164853+MadsPalu@users.noreply.github.com>
2026-03-02 14:57:25 +0100

    Tune parallel cracking and add runtime worker controls

--- PasswordCrackerCentralized/PasswordCrackerCentralized/Cracking.cs
+++ PasswordCrackerCentralized/PasswordCrackerCentralized/Cracking.cs
@@ -1,26 +1,25 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 using PasswordCrackerCentralized.model;
 using PasswordCrackerCentralized.util;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Security.Cryptography;
 
 namespace PasswordCrackerCentralized
 {
     public class Cracking
     {
-        /// <summary>
-        /// The algorithm used for encryption.
-        /// Must be exactly the same algorithm that was used to encrypt the passwords in the password file
-        /// </summary>
-        private readonly HashAlgorithm _messageDigest;
+        private readonly int _workerCount;
+        private readonly bool _verbose;
 
-        public Cracking()
+        public Cracking(int? workerCount = null, bool verbose = false)
         {
-            _messageDigest = new SHA1CryptoServiceProvider();
-            //_messageDigest = new MD5CryptoServiceProvider();
-            // seems to be same speed
+            _workerCount = workerCount ?? Environment.ProcessorCount;
+            _verbose = verbose;
         }
 
         /// <summary>
@@ -30,137 +29,122 @@ namespace PasswordCrackerCentralized
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
-            List<UserInfo> userInfos =
-                PasswordFileHandler.ReadPasswordFile("passwords.txt");
-            Console.WriteLine("passwd opeend");
+            List<UserInfo> userInfos = PasswordFileHandler.ReadPasswordFile("passwords.txt");
+            Console.WriteLine("passwd opened");
 
-            List<UserInfoClearText> result = new List<UserInfoClearText>();
+            Dictionary<string, List<UserInfo>> usersByHash = BuildUsersByHash(userInfos);
+            ConcurrentDictionary<string, UserInfoClearText> crackedUsers =
+                new ConcurrentDictionary<string, UserInfoClearText>(StringComparer.Ordinal);
 
-            using (FileStream fs = new FileStream("webster-dictionary.txt", FileMode.Open, FileAccess.Read))
-
-            using (StreamReader dictionary = new StreamReader(fs))
+            IEnumerable<string> dictionaryEntries = File.ReadLines("webster-dictionary.txt");
+            ParallelOptions parallelOptions = new ParallelOptions
             {
-                while (!dictionary.EndOfStream)
+                MaxDegreeOfParallelism = _workerCount
+            };
+
+            // Master/worker setup: master feeds dictionary entries, workers test all word variations.
+            Parallel.ForEach(
+                dictionaryEntries,
+                parallelOptions,
+                () => (HashAlgorithm) new SHA1CryptoServiceProvider(),
+                (dictionaryEntry, loopState, index, messageDigest) =>
                 {
-                    String dictionaryEntry = dictionary.ReadLine();
-                    IEnumerable<UserInfoClearText> partialResult = CheckWordWithVariations(dictionaryEntry, userInfos);
-                    result.AddRange(partialResult);
-                }
-            }
+                    CheckWordWithVariations(dictionaryEntry, usersByHash, crackedUsers, messageDigest);
+                    return messageDigest;
+                },
+                messageDigest => messageDigest.Dispose());
+
+            List<UserInfoClearText> result = crackedUsers.Values
+                .OrderBy(entry => entry.Username)
+                .ToList();
+
             stopwatch.Stop();
             Console.WriteLine(string.Join(", ", result));
             Console.WriteLine("Out of {0} password {1} was found ", userInfos.Count, result.Count);
             Console.WriteLine();
+            Console.WriteLine("Workers used: {0}", _workerCount);
             Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
         }
 
-        /// <summary>
-        /// Generates a lot of variations, encrypts each of the and compares it to all entries in the password file
-        /// </summary>
-        /// <param name="dictionaryEntry">A single word from the dictionary</param>
-        /// <param name="userInfos">List of (username, encrypted password) pairs from the password file</param>
-        /// <returns>A list of (username, readable password) pairs. The list might be empty</returns>
-        private IEnumerable<UserInfoClearText> CheckWordWithVariations(String dictionaryEntry, List<UserInfo> userInfos)
+        private static Dictionary<string, List<UserInfo>> BuildUsersByHash(IEnumerable<UserInfo> userInfos)
         {
-            List<UserInfoClearText> result = new List<UserInfoClearText>(); //might be empty
+            Dictionary<string, List<UserInfo>> usersByHash = new Dictionary<string, List<UserInfo>>(StringComparer.Ordinal);
 
-            String possiblePassword = dictionaryEntry;
-            IEnumerable<UserInfoClearText> partialResult = CheckSingleWord(userInfos, possiblePassword);
-            result.AddRange(partialResult);
+            foreach (UserInfo userInfo in userInfos)
+            {
+                string hashKey = Convert.ToBase64String(userInfo.EntryptedPassword.ToArray());
 
-            String possiblePasswordUpperCase = dictionaryEntry.ToUpper();
-            IEnumerable<UserInfoClearText> partialResultUpperCase = CheckSingleWord(userInfos, possiblePasswordUpperCase);
-            result.AddRange(partialResultUpperCase);
+                if (!usersByHash.TryGetValue(hashKey, out List<UserInfo> usersWithSameHash))
+                {
+                    usersWithSameHash = new List<UserInfo>();
+                    usersByHash[hashKey] = usersWithSameHash;
+                }
 
-            String possiblePasswordCapitalized = StringUtilities.Capitalize(dictionaryEntry);
-            IEnumerable<UserInfoClearText> partialResultCapitalized = CheckSingleWord(userInfos, possiblePasswordCapitalized);
-            result.AddRange(partialResultCapitalized);
+                usersWithSameHash.Add(userInfo);
+            }
 
-            String possiblePasswordReverse = StringUtilities.Reverse(dictionaryEntry);
-            IEnumerable<UserInfoClearText> partialResultReverse = CheckSingleWord(userInfos, possiblePasswordReverse);
-            result.AddRange(partialResultReverse);
+            return usersByHash;
+        }
+
+        /// <summary>
+        /// Generates a lot of variations, encrypts each and compares to all entries in the password file
+        /// </summary>
+        private void CheckWordWithVariations(
+            string dictionaryEntry,
+            IReadOnlyDictionary<string, List<UserInfo>> usersByHash,
+            ConcurrentDictionary<string, UserInfoClearText> crackedUsers,
+            HashAlgorithm messageDigest)
+        {
+            CheckSingleWord(usersByHash, crackedUsers, dictionaryEntry, messageDigest);
+            CheckSingleWord(usersByHash, crackedUsers, dictionaryEntry.ToUpper(), messageDigest);
+            CheckSingleWord(usersByHash, crackedUsers, StringUtilities.Capitalize(dictionaryEntry), messageDigest);
+            CheckSingleWord(usersByHash, crackedUsers, StringUtilities.Reverse(dictionaryEntry), messageDigest);
 
             for (int i = 0; i < 100; i++)
             {
-                String possiblePasswordEndDigit = dictionaryEntry + i;
-                IEnumerable<UserInfoClearText> partialResultEndDigit = CheckSingleWord(userInfos, possiblePasswordEndDigit);
-                result.AddRange(partialResultEndDigit);
+                CheckSingleWord(usersByHash, crackedUsers, dictionaryEntry + i, messageDigest);
             }
 
             for (int i = 0; i < 100; i++)
             {
-                String possiblePasswordStartDigit = i + dictionaryEntry;
-                IEnumerable<UserInfoClearText> partialResultStartDigit = CheckSingleWord(userInfos, possiblePasswordStartDigit);
-                result.AddRange(partialResultStartDigit);
+                CheckSingleWord(usersByHash, crackedUsers, i + dictionaryEntry, messageDigest);
             }
 
             for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    String possiblePasswordStartEndDigit = i + dictionaryEntry + j;
-                    IEnumerable<UserInfoClearText> partialResultStartEndDigit = CheckSingleWord(userInfos, possiblePasswordStartEndDigit);
-                    result.AddRange(partialResultStartEndDigit);
+                    CheckSingleWord(usersByHash, crackedUsers, i + dictionaryEntry + j, messageDigest);
                 }
             }
-
-            return result;
         }
 
         /// <summary>
-        /// Checks a single word (or rather a variation of a word): Encrypts and compares to all entries in the password file
+        /// Checks a single candidate by hashing it once and doing O(1) lookup in the encrypted-password index
         /// </summary>
-        /// <param name="userInfos"></param>
-        /// <param name="possiblePassword">List of (username, encrypted password) pairs from the password file</param>
-        /// <returns>A list of (username, readable password) pairs. The list might be empty</returns>
-        private IEnumerable<UserInfoClearText> CheckSingleWord(IEnumerable<UserInfo> userInfos, String possiblePassword)
+        private void CheckSingleWord(
+            IReadOnlyDictionary<string, List<UserInfo>> usersByHash,
+            ConcurrentDictionary<string, UserInfoClearText> crackedUsers,
+            string possiblePassword,
+            HashAlgorithm messageDigest)
         {
             char[] charArray = possiblePassword.ToCharArray();
             byte[] passwordAsBytes = Array.ConvertAll(charArray, PasswordFileHandler.GetConverter());
+            byte[] encryptedPassword = messageDigest.ComputeHash(passwordAsBytes);
+            string encryptedPasswordKey = Convert.ToBase64String(encryptedPassword);
 
-            byte[] encryptedPassword = _messageDigest.ComputeHash(passwordAsBytes);
-            //string encryptedPasswordBase64 = System.Convert.ToBase64String(encryptedPassword);
-
-            List<UserInfoClearText> results = new List<UserInfoClearText>();
-
-            foreach (UserInfo userInfo in userInfos)
+            if (!usersByHash.TryGetValue(encryptedPasswordKey, out List<UserInfo> matches))
             {
-                if (CompareBytes(userInfo.EntryptedPassword, encryptedPassword))  //compares byte arrays
+                return;
+            }
+
+            foreach (UserInfo userInfo in matches)
+            {
+                if (crackedUsers.TryAdd(userInfo.Username, new UserInfoClearText(userInfo.Username, possiblePassword)) && _verbose)
                 {
-                    results.Add(new UserInfoClearText(userInfo.Username, possiblePassword));
                     Console.WriteLine(userInfo.Username + " " + possiblePassword);
                 }
             }
-            return results;
         }
-
-        /// <summary>
-        /// Compares to byte arrays. Encrypted words are byte arrays
-        /// </summary>
-        /// <param name="firstArray"></param>
-        /// <param name="secondArray"></param>
-        /// <returns></returns>
-        private static bool CompareBytes(IList<byte> firstArray, IList<byte> secondArray)
-        {
-            //if (secondArray == null)
-            //{
-            //    throw new ArgumentNullException("firstArray");
-            //}
-            //if (secondArray == null)
-            //{
-            //    throw new ArgumentNullException("secondArray");
-            //}
-            if (firstArray.Count != secondArray.Count)
-            {
-                return false;
-            }
-            for (int i = 0; i < firstArray.Count; i++)
-            {
-                if (firstArray[i] != secondArray[i])
-                    return false;
-            }
-            return true;
-        }
-
     }
 }
